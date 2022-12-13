39afc1803a926ada3938cadf46d28b4e8f1a5cec
Cagatay Gevrek <cagataygevrek@gmail.com>
2022-12-13 23:01:16 +0300

    component açıklandı.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+// root adında sabit oluşturulup index.html'deki id'si root olan div seçilerek App komponenti render edilmiş.
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
